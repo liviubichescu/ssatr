2d7e16d2895468386cf87716be9134aed1993565
Liviu Bichescu <liviubichescu@gmail.com>
2020-11-09 20:44:33 +0200

    ex.5 resolved

--- src/main/java/com/mycompany/ssatr/bank/AccountsManager.java
+++ src/main/java/com/mycompany/ssatr/bank/AccountsManager.java
@@ -5,13 +5,28 @@
  */
 package com.mycompany.ssatr.bank;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author liviu.bichescu
  */
-public class AccountsManager {
+public class AccountsManager {  
     
-    private void addAccount(BankAccount a){
-
+    ArrayList<BankAccount> accounts = new ArrayList<BankAccount>(); 
+    
+    public void addAccount(BankAccount a){
+        this.accounts.add(a);
+    }
+    
+    public int getTotalBalance(){
+        
+        int totalAccount = 0;
+        
+        for(BankAccount acc : this.accounts){
+            totalAccount = totalAccount + acc.getBalance();
+        }
+        
+        return totalAccount;
     }
 }
